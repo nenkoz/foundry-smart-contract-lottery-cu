a0686bb0d743c985fc729535e01591785e9343d3
nenkoz <nenko@cantab.net>
2024-11-10 04:27:35 +0000

    testing raffle reverts when fee not enough and event emission when new player enters

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -10,6 +10,8 @@ contract RaffleTest is Test {
     Raffle public raffle;
     HelperConfig public helperConfig;
 
+
+
     uint256 entranceFee;
     uint256 interval;
     address vrfCoordinator;
@@ -20,6 +22,8 @@ contract RaffleTest is Test {
     address public PLAYER = makeAddr("player"); // foundry cheatcoat to make an address from a string
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
 
+    event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
     function setUp() external {
         DeployRaffle deployRaffle = new DeployRaffle();
@@ -31,9 +35,35 @@ contract RaffleTest is Test {
         gasLane = config.gasLane;
         subscriptionId = config.subscriptionId;
         callbackGasLimit = config.callbackGasLimit;
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
     }
 
     function testRaffleInitializesInOpenState() public view {
         assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    function testRaffleRevertsWhenYouDontPayTheEntranceFee() public {
+        // Arrange
+        vm.prank(PLAYER);
+        // Act / Assert
+        vm.expectRevert(Raffle.Raffle__SendMoreToEnterRaffle.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        // Arrange
+        vm.prank(PLAYER);
+
+        // Act
+        raffle.enterRaffle{value: entranceFee}();
+        // Assert
+        assert(raffle.getPlayers(0) == PLAYER);
+    }
+
+    function testEnteringRaffleEmitsEvent() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false, address(raffle));
+        emit RaffleEntered(address(0));
+        raffle.enterRaffle{value: entranceFee}();
     }
 }
